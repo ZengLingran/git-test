4e6963e74e2dc8c0ef6b280863d81966540e9a51
ZengLR <898802801@qq.com>
2019-11-29 02:12:58 +0800

    第三方人员下载项目后，修改再提交

--- login/src/main/java/com/shop/web/hello.java
+++ login/src/main/java/com/shop/web/hello.java
@@ -7,5 +7,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git222222222222222");
+        System.out.println("其他开发人员修改git");
     }
 }
