9c70c7f37d22520ec922437b195f78b8088a5e31
ZengLR <898802801@qq.com>
2019-11-29 01:49:30 +0800

    把本地工程添加到git

---
+++ login/src/main/java/com/shop/web/hello.java
@@ -0,0 +1,10 @@
+package com.shop.web;
+
+/**
+ * Created by Computer on 2019/11/29.
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
